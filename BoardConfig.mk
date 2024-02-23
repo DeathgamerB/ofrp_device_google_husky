2a060fdfccadeb009b9508cfaa0b110e6c53d6e4
DeathgamerB <87858321+DeathgamerB@users.noreply.github.com>
2024-02-23 15:01:23 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,7 @@ TARGET_KERNEL_SOURCE := kernel/google/generic
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_KERNEL := device/google/husky/kernel
 endif
 
 # Partitions
