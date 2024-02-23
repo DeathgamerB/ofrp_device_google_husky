693208d975839f123dd4c350f52f04df197e3a77
DeathgamerB <87858321+DeathgamerB@users.noreply.github.com>
2024-02-23 14:22:01 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/google/generic
+LOCAL_PATH := device/google/husky
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
