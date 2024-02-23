da1dd2c90fce2a79a2574832d260a17da1852e96
DeathgamerB <87858321+DeathgamerB@users.noreply.github.com>
2024-02-23 21:47:05 +0100

    Update omni_husky.mk

--- omni_husky.mk
+++ omni_husky.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit from generic device
 $(call inherit-product, device/google/husky/device.mk)
 
-PRODUCT_DEVICE := generic
+PRODUCT_DEVICE := husky
 PRODUCT_NAME := omni_husky
 PRODUCT_BRAND := google
 PRODUCT_MODEL := mainline
